90ebe4630b73765178d43e81b88aba3ced70d424
Adrian Ulrich <adrian@blinkenlights.ch>
2014-05-04 22:21:16 +0200

    libril: Send fake event for manual network registration to unblock UI
     [AdrianDC] Port to AOSP O new RIL stack
    
    Change-Id: If3b3b0cd82a62a6b4a827ac9ce9621c26ec56459
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- libril/ril_service.cpp
+++ libril/ril_service.cpp
@@ -1310,6 +1310,22 @@ Return<void> RadioImpl::setNetworkSelectionModeManual(int32_t serial,
 #endif
     dispatchString(serial, mSlotId, RIL_REQUEST_SET_NETWORK_SELECTION_MANUAL,
             operatorNumeric.c_str());
+
+    /**
+      * Qualcomm's RIL doesn't seem to issue any callbacks for opcode 47
+      * This may be a bug on how we call rild or simply some proprietary 'feature'
+      * ..and we don't care: We simply send a SUCCESS message back to the caller to
+      * indicate that we received the command & unblock the UI.
+      * The user will still see if the registration was OK by using the
+      * normal signal meter
+      */
+    RLOGE("setNetworkSelectionModeManual: sending fake success event");
+    RequestInfo *pRI = android::addRequestToList(serial, mSlotId,
+            RIL_REQUEST_SET_NETWORK_SELECTION_MANUAL);
+    if (pRI != NULL) {
+        RIL_onRequestComplete(pRI, RIL_E_SUCCESS, NULL, 0);
+    }
+
     return Void();
 }
 
