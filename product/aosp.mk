a4b8173fa9be3ad6fbb8d9b18c9f193c8aafb8e5
Adrian DC <radian.dc@gmail.com>
2016-08-25 03:43:29 +0200

    build: Add AOSP optional products and properties
     * Builds non default AOSP products:
        Launcher3, messaging, SoundRecorder,
        SpeechRecorder, Terminal
    
     * Prevent Google SetupWizard from rotating
     * Show storage manager settings
    
     * Add default overlay overrides for Settings
        gestures and storage manager visibility and
        enable productivity_device for DocumentsUI
    
    Change-Id: I830c861404ff5ce943507952ea1c31088b9aca97
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

---
+++ product/aosp.mk
@@ -0,0 +1,17 @@
+# AOSP specific products
+ifneq ($(BOARD_AOSP_BASED),)
+
+# AOSP packages
+PRODUCT_PACKAGES += \
+    Launcher3 \
+    messaging \
+    SoundRecorder \
+    SpeechRecorder \
+    Terminal
+
+# AOSP properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.setupwizard.rotation_locked=true \
+    ro.storage_manager.enabled=true
+
+endif
