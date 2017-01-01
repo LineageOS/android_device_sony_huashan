bc04b7cb3c682a1bd30a2cbc0c42db0d0c171ffe
Adrian DC <radian.dc@gmail.com>
2017-01-01 18:45:52 +0100

    huashan: proprietary: Use new generic extract-utils
     * Based on similar scripts from nicki, jf, ether
     * Keep common qcom binaries for msm8960 in comments
    
     * Updated with the removed unreferenced libraries
    
    Change-Id: I26c4cddff9412c00e2d40ecbc0209fac22d42eef
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -1,15 +1,67 @@
 #!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#           (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Use tradition sort
-export LC_ALL=C
+set -e
 
-FP=$(cd ${0%/*} && pwd -P)
-export VENDOR=$(basename $(dirname $FP))
-export DEVICE=$(basename $FP)
-export BOARDCONFIGVENDOR=true
+# Required!
+export DEVICE=huashan
+export VENDOR=sony
 
-../common/extract-files.sh $@
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-../common/setup-makefiles.sh
+ROM_ROOT="$MY_DIR"/../../..
 
-./setup-makefiles.sh
+HELPER="$ROM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+if [ $# -eq 0 ]; then
+    SRC=adb
+else
+    if [ $# -eq 1 ]; then
+        SRC=$1
+    else
+        echo "$0: bad number of arguments"
+        echo ""
+        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+        echo ""
+        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+        echo "the device using adb pull."
+        exit 1
+    fi
+fi
+
+# Initialize the helper for device
+setup_vendor "$DEVICE" "$VENDOR" "$ROM_ROOT"
+
+# Sony/Device specific blobs
+extract "$MY_DIR"/proprietary-files-sony.txt "$SRC"
+
+# QCom common blobs
+extract "$MY_DIR"/proprietary-files-qc.txt "$SRC"
+
+# Generate vendor makefiles
+"$MY_DIR"/setup-makefiles.sh
+
+# QCom Common binaries for msm8960
+# "$ROM_ROOT"/device/qcom/common/extractors/extract-files.sh "$SRC" msm8960 graphics
