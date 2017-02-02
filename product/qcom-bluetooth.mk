a5079e6df4e4eaa415c32114912155d1558b9484
Myles Watson <mylesgw@google.com>
2017-02-02 11:33:19 -0800

    bluetooth: Build the new Bluetooth HAL
    Change-Id: Ibeb69b0764ee6ca3244d4026d69b80fd8ec368b9

--- product/qcom-bluetooth.mk
+++ product/qcom-bluetooth.mk
@@ -1,3 +1,7 @@
 # Bluetooth permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+
+# Bluetooth HAL
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
