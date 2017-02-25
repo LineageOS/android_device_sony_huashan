fc8fda974c2353d5ec18e46f5b9e90a76823014a
Adrian DC <radian.dc@gmail.com>
2017-02-25 09:05:48 +0100

    huashan: recovery: Add support for TWRP 3.x
     * Provide the configuration based on official releases
     * Provide TWRP fstab with misc partition support
     * Switch to TWRP settings by exporting WITH_TWRP=true
     * Move regular recovery configs to recovery.mk
    
    Change-Id: I37663f0632249451a30db7f98efe1e0c74c69941
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,7 +1,12 @@
-# Recovery
+# TWRP (Optional)
+ifneq ($(WITH_TWRP),true)
+
+# Recovery configurations
 RECOVERY_FSTAB_VERSION := 2
 ifneq ($(BOARD_AOSP_BASED),)
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab_aosp.qcom
 else
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
+endif
+
 endif
