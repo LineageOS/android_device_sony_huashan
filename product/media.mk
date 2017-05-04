652a7d711b4c085e1e422466ca6efd5f4dbb5320
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    media: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Test: Compiles
    
    Bug: 36430270
    
    From: Pawin Vongmasa <pawin@google.com>
    Date: Wed May 03 22:42:57 2017 -0700
    
        Remove the property media.settings.xml.
    
        This CL also removes media_profiles.xml as it should be replaced by
        media_profiles_V1_0.xml.
    
        Bug: 36430270
        Merged-In: I4299abfde44ced36560736520d0be592894d00a7
        Change-Id: I4299abfde44ced36560736520d0be592894d00a7
    
    Merged-In: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    Change-Id: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c
    Signed-off-by: Chippa-a <vusal1372@gmail.com>
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/media.mk
+++ product/media.mk
@@ -8,8 +8,4 @@ PRODUCT_COPY_FILES += \
 endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
-
-# Media properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.settings.xml=/vendor/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
