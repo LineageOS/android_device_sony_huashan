adc9409bbed381444cbb7d11bf5e1ada665d2fc8
Adrian DC <radian.dc@gmail.com>
2017-07-24 16:41:53 +0200

    libhealthd: Add libbase static library
     * Also cleanup Android.mk related rules
    
    Change-Id: Id7ba35842fa089ceaf832c8eb3a50d78b4bf8cc8
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -5,9 +5,17 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := healthd_board_msm.cpp
-LOCAL_MODULE := libhealthd.board
-LOCAL_WHOLE_STATIC_LIBRARIES := libcutils
 LOCAL_C_INCLUDES := system/core/healthd/include
+
+LOCAL_MODULE := libhealthd.board
 LOCAL_CFLAGS := -Werror
+
+LOCAL_STATIC_LIBRARIES := \
+    libbase
+
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+    libcutils
+
 include $(BUILD_STATIC_LIBRARY)
