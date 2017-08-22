ee3ecabefd68cf0179b5cfaf8f4608f9d6d22f88
Adrian DC <radian.dc@gmail.com>
2017-08-22 13:30:23 +0200

    hidl: Add USB HAL interfaces package
    Change-Id: Idbc80071587cdee4c9a98f1156204d9ac1ed074a
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/qcom-usb.mk
+++ product/qcom-usb.mk
@@ -2,3 +2,7 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+# USB HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
