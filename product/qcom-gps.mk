459ca3df3600c08fd6f716ad0e8cd7959cca4cb9
Adrian DC <radian.dc@gmail.com>
2017-08-22 13:30:23 +0200

    hidl: Add GPS HAL interfaces needed for msm8960
    Change-Id: Ic76354aa13234d44ab76829284b90e611d8a3973
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,3 +1,7 @@
 # GPS permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
+
+# GPS HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl
