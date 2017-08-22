720a5bdbdb60ae0560fab89c410a4090333ecb87
Adrian DC <radian.dc@gmail.com>
2017-08-22 13:30:23 +0200

    hidl: Add display HAL interfaces legacy Adreno 320
    Change-Id: I043c5ca64f3aaceb219a2bf974ae2df823df234e
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -5,6 +5,13 @@ PRODUCT_PACKAGES += \
     copybit.msm8960 \
     memtrack.msm8960
 
+# Display HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl
+
 # OpenGL properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196608
