8df01c93bb3ae83cce77811a240f8c2bf8390908
Adrian DC <radian.dc@gmail.com>
2017-08-22 13:30:23 +0200

    hidl: Add Camera HAL interfaces package
    Change-Id: I999990f3b9be63ecd2b5df103e23a7db4bcc04fb
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -2,3 +2,8 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml
+
+# Camera HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl
