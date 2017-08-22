1db132b85b84a91178d5635dc48166dc2c361b50
Adrian DC <radian.dc@gmail.com>
2017-08-22 13:30:23 +0200

    hidl: Add audio HAL packages needed for msm8960
    Change-Id: Ia042117656c6abae9dc4baabf571857c7cc7fd52
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -14,3 +14,8 @@ PRODUCT_PACKAGES += \
 # Audio tools
 PRODUCT_PACKAGES += \
     tinymix
+
+# Audio HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl
