17666a5badc9c94649bf9ad6b1d1f5c59ff6e89c
Adrian DC <radian.dc@gmail.com>
2017-08-22 13:30:23 +0200

    hidl: Match the other HIDL inclusions style
    Change-Id: I081b286a041ad25727cdcbc8144cbee35611cbc2
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/qcom-bluetooth.mk
+++ product/qcom-bluetooth.mk
@@ -2,7 +2,10 @@
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
-# Bluetooth HAL
+# Bluetooth packages
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
     libbt-vendor
+
+# Bluetooth HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
