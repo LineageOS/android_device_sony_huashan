171294072d5728835effde9c0076b7f1374e1a6a
Adrian DC <radian.dc@gmail.com>
2017-08-22 13:30:23 +0200

    hidl: Match the other HIDL inclusions style
    Change-Id: I081b286a041ad25727cdcbc8144cbee35611cbc2
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/qcom-nfc.mk
+++ product/qcom-nfc.mk
@@ -3,10 +3,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
-# NFC HAL
-PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl
-
 # NFC packages
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
@@ -14,3 +10,7 @@ PRODUCT_PACKAGES += \
     libnfc_jni \
     Nfc \
     Tag
+
+# NFC HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl
