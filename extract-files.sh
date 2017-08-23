7834cc6c85d26f600817e003775eb04bb29e67ed
Bruno Martins <bgcngm@gmail.com>
2017-08-23 14:43:23 +0100

    extract-files: Update extractors to use Lineage vendor
     * Match the branding change as well
    
    Change-Id: Ie39f8f52f3a7d552c82c0da643732af447aeb91e

--- extract-files.sh
+++ extract-files.sh
@@ -26,9 +26,9 @@ export VENDOR=sony
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-ROM_ROOT="$MY_DIR"/../../..
+LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$ROM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -58,7 +58,7 @@ if [ -z "$SRC" ]; then
 fi
 
 # Initialize the helper for device
-setup_vendor "$DEVICE" "$VENDOR" "$ROM_ROOT" false "$CLEAN_VENDOR"
+setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
 
 # Sony/Device specific blobs
 extract "$MY_DIR"/proprietary-files-sony.txt "$SRC" "$SECTION"
