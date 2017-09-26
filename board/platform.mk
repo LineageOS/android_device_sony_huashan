c8ea7230e5fd03fd13d4339bdf39d5baf91754fa
Chippa-a <vusal1372@gmail.com>
2017-09-26 15:44:37 +0300

    dumpstate: Convert to hidl implementation
    Based on implementation for angler (7fab099).
    
    Includes:
    "Uses libdumpstateheaders instead of LOCAL_C_INCLUDES" (60e7a0f)
    "Uses new Dumpstate class functions on dumpstate_board()" (d75e6a6)
    "Mark hal_dumpstate_impl as HAL impl domain" (66957ed)
    "Switch Dumpstate HAL to hal_server_domain " (c3b9841)
    "dumpstate HAL uses "default" service name()" (5b8189a)
    "Check result of registerAsService in dumpstate." (cf2996a)
    
    Change-Id: I91e0caeb4964a85e8d43d2aad86acd65b1cc2488

--- board/platform.mk
+++ board/platform.mk
@@ -8,9 +8,6 @@ BOARD_VENDOR_PLATFORM := viskan
 # Legacy blobs
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
 
-# Dumpstate
-BOARD_LIB_DUMPSTATE := libdumpstate.sony
-
 # Images
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
