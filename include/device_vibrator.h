52ca61ea15708afc23d878b968bf740fb88f09f8
Chippa-a <vusal1372@gmail.com>
2017-09-30 20:35:40 +0300

    vibrator: Add custom service for Oreo
    Based on implementation for marlin (1ca70b4).
    
    Includes:
    "Vibrator service: die if not setup" (2b6801a)
    "Remove readproc group from vibrator" (d00cd01)
    "Add support for EFFECT_CLICK." (0df7f65)
    
    Additional changes:
    - changed 'voltage' to 'intensity'
    - back to Android.mk
    - move some definitions to separate header
    
    Configured for huashan to match old CMHW values.
    
    Change-Id: Ie74465469b4fe3113a8597e96a17f82d46cba220

---
+++ include/device_vibrator.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+static constexpr int MAX_INTENSITY = 31;
+static constexpr int MIN_INTENSITY = 19;
+static constexpr uint32_t CLICK_TIMING_MS = 50;
+static constexpr uint32_t LIGHT_AMPLITUDE = 36;
+static constexpr uint32_t MEDIUM_AMPLITUDE = 128;
+static constexpr uint32_t STRONG_AMPLITUDE = 255;
