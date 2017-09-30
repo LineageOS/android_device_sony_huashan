38ada4792acb4a87ddc9ac04270f6a51f2d4c9e3
Chippa-a <vusal1372@gmail.com>
2017-09-30 20:35:40 +0300

    vibrator: Add custom service for Oreo
    Based on implementation for marlin (1ca70b4).
    
    Includes:
    "Vibrator service: die if not setup" (2b6801a)
    "Remove readproc group from vibrator" (d00cd01)
    "Add support for EFFECT_CLICK." (0df7f65)
    
    Additional changes:
    - changed 'voltage' to 'intensity'
    - back to Android.mk
    - move some definitions to separate header
    
    Configured for huashan to match old CMHW values.
    
    Change-Id: Ie74465469b4fe3113a8597e96a17f82d46cba220

---
+++ vibrator/Vibrator.cpp
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "VibratorService"
+
+#include <log/log.h>
+
+#include <hardware/hardware.h>
+#include <hardware/vibrator.h>
+
+#include "Vibrator.h"
+
+#include <cinttypes>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+namespace android {
+namespace hardware {
+namespace vibrator {
+namespace V1_0 {
+namespace implementation {
+
+Vibrator::Vibrator(std::ofstream&& enable, std::ofstream&& amplitude) :
+        mEnable(std::move(enable)),
+        mAmplitude(std::move(amplitude)) {}
+
+// Methods from ::android::hardware::vibrator::V1_0::IVibrator follow.
+Return<Status> Vibrator::on(uint32_t timeout_ms) {
+    mEnable << timeout_ms << std::endl;
+    if (!mEnable) {
+        ALOGE("Failed to turn vibrator on (%d): %s", errno, strerror(errno));
+        return Status::UNKNOWN_ERROR;
+    }
+    return Status::OK;
+}
+
+Return<Status> Vibrator::off()  {
+    mEnable << 0 << std::endl;
+    if (!mEnable) {
+        ALOGE("Failed to turn vibrator off (%d): %s", errno, strerror(errno));
+        return Status::UNKNOWN_ERROR;
+    }
+    return Status::OK;
+}
+
+Return<bool> Vibrator::supportsAmplitudeControl()  {
+    return true;
+}
+
+Return<Status> Vibrator::setAmplitude(uint8_t amplitude) {
+    if (amplitude == 0) {
+        return Status::BAD_VALUE;
+    }
+    // Scale the intensity such that an amplitude of 1 is MIN_INTENSITY, an amplitude of 255 is
+    // MAX_INTENSITY, and there are equal steps for every value in between.
+    long intensity =
+            std::lround((amplitude - 1) / 254.0 * (MAX_INTENSITY - MIN_INTENSITY) + MIN_INTENSITY);
+    ALOGE("Setting amplitude  to: %ld", intensity);
+    mAmplitude << intensity << std::endl;
+    if (!mAmplitude) {
+        ALOGE("Failed to set amplitude (%d): %s", errno, strerror(errno));
+        return Status::UNKNOWN_ERROR;
+    }
+    return Status::OK;
+}
+
+Return<void> Vibrator::perform(Effect effect, EffectStrength strength, perform_cb _hidl_cb) {
+    if (effect == Effect::CLICK) {
+        uint8_t amplitude;
+        switch (strength) {
+        case EffectStrength::LIGHT:
+            amplitude = LIGHT_AMPLITUDE;
+            break;
+        case EffectStrength::MEDIUM:
+            amplitude = MEDIUM_AMPLITUDE;
+            break;
+        case EffectStrength::STRONG:
+            amplitude = STRONG_AMPLITUDE;
+            break;
+        default:
+            _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
+            return Void();
+        }
+        on(CLICK_TIMING_MS);
+        setAmplitude(amplitude);
+        _hidl_cb(Status::OK, CLICK_TIMING_MS);
+    } else {
+        _hidl_cb(Status::UNSUPPORTED_OPERATION, 0);
+    }
+    return Void();
+}
+
+} // namespace implementation
+}  // namespace V1_0
+}  // namespace vibrator
+}  // namespace hardware
+}  // namespace android
