6e9db8ae3941896ca6c210895016d17bdef8c372
Bruno Martins <bgcngm@gmail.com>
2017-10-06 14:32:59 +0100

    DeviceSettings: Build with AAPT2
    Change-Id: I58ee547ff1dd115bff65925e05ce5530ddfe6290

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -9,7 +9,9 @@ LOCAL_PACKAGE_NAME := DeviceSettings
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v14-preference \
     android-support-v7-appcompat \
     android-support-v7-palette \
@@ -25,14 +27,7 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res \
-    frameworks/support/v14/preference/res \
-    frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/preference/res \
-    frameworks/support/v7/recyclerview/res
-
-LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages android.support.v14.preference:android.support.v7.appcompat:android.support.v7.preference:android.support.v7.recyclerview
+    $(TOP)/packages/resources/devicesettings/res
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
