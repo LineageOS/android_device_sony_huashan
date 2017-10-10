804a6b93165f13e87accbe7673c73aff05e0e355
Nirmal Abraham <nabrah@codeaurora.org>
2017-10-10 13:51:47 +0530

    display: Disable SF Latch Buffer Signaled property
    Change-Id: I682b30fbefdfbd4d8478a82148d5984138078957

--- product/display.mk
+++ product/display.mk
@@ -2,5 +2,6 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.use_buffer_age=false \
     debug.sf.enable_gl_backpressure=1 \
+    debug.sf.latch_unsignaled=1 \
     persist.hwc.mdpcomp.enable=true \
     ro.sf.lcd_density=320
