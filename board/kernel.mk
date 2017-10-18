4fd19f1cc249a099b980b8d47912de4c9cb248e4
Adrian DC <radian.dc@gmail.com>
2017-10-18 23:50:43 +0200

    board: Define kernel zImage name
     * Automated detection removed in following commit:
        https://review.lineageos.org/#/c/192397/
    
    Change-Id: I613c1756f52d78e5af9033a810a86968a0f3623e

--- board/kernel.mk
+++ board/kernel.mk
@@ -2,6 +2,7 @@
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := # Ignored, see cmdline.txt
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 # Kernel properties
