ea3aa13aff99a73f8f9578e4661c80689357f158
Adrian DC <radian.dc@gmail.com>
2017-12-06 20:51:11 +0100

    build: Use Android Go configurations
    Change-Id: I877caf945d6ca8556494a0b623f383ea30b28c8b
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- aosp_huashan.mk
+++ aosp_huashan.mk
@@ -18,6 +18,9 @@
 # Device configurations for AOSP
 BOARD_AOSP_BASED := true
 
+# Inherit common Android Go configurations
+$(call inherit-product, build/target/product/go_defaults.mk)
+
 # Inherit device configurations
 $(call inherit-product, device/sony/huashan/device.mk)
 
