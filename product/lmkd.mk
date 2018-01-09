0ddc78376da1278d388ce8e389c8bdc15934c42e
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    lmkd: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

---
+++ product/lmkd.mk
@@ -0,0 +1,10 @@
+# LMKD properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.critical=0 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=0 \
+    ro.lmk.upgrade_pressure=100
