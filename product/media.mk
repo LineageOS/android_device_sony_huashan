095514f29331d24e0a88463d202013e99c23590e
Adrian DC <radian.dc@gmail.com>
2018-02-17 23:55:34 +0100

    media: Use AOSP's media_profiles.xml configuration
     * Re-unify the file under media_profiles.xml
        without the unsupported profiles
    
     * Introduced by commit :
        "audio: Provide AOSP specific audio and media configurations"
        Change-Id: I6d8c2527b7e480e153428ec78f41809f7ff2aa28
        Date: Sat, 22 Apr 2017 12:44:48 +0200
    
    Change-Id: Ia1cfd4529016e0c7575d85ba42a4caa5859ae6ab

--- product/media.mk
+++ product/media.mk
@@ -1,12 +1,11 @@
 # Media configurations
 ifneq ($(BOARD_AOSP_BASED),)
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs_performance_aosp.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles_aosp.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_codecs_performance_aosp.xml:system/etc/media_codecs_performance.xml
 else
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 endif
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
