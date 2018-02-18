da51ee7a479f5db878d0774d0ddc58555a2a2769
Adrian DC <radian.dc@gmail.com>
2018-02-18 14:35:54 +0100

    shim: Split TARGET_LD_SHIM_LIBS on multiple lines
     * Follows the vendor/lineage commit:
        "lineage: soong: replace space with colon on TARGET_LD_SHIM_LIBS"
        From: Stricted <info@stricted.net>
        Date: Sat, 17 Feb 2018 17:28:52 +0100
        Change-Id: Ice1411ee22b78da1dbf22eac412b9e4978acebdf
    
    Change-Id: Ibfbf2a8025183c138ac9c0719a9352620f2c839c

--- board/shim.mk
+++ board/shim.mk
@@ -1,2 +1,4 @@
 # SHIM
-TARGET_LD_SHIM_LIBS := /system/lib/libcald_pal.so|libshim_cald.so:/system/lib/hw/camera.vendor.qcom.so|libshim_camera.so
+TARGET_LD_SHIM_LIBS := \
+    /system/lib/libcald_pal.so|libshim_cald.so \
+    /system/lib/hw/camera.vendor.qcom.so|libshim_camera.so
