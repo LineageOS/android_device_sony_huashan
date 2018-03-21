9d3a86852095882d61dc1496f204a9d28289c49d
Adrian DC <radian.dc@gmail.com>
2018-03-22 00:32:41 +0100

    camera: libstagefright: Disable camcorder metadata
     * Refer to the following commit:
    
        Author: Adrian DC <radian.dc@gmail.com>
        Date:   Tue, 20 Mar 2018 23:48:08 +0100
        libstagefright: Support disabling metadata with a property
        Change-Id: Ia5f3bd30cd24bd2d10c9c145c91d4b616f4edb44
    
    Change-Id: Idfda05fa984181c931ef324788985e2aa2217ec2

--- product/camera.mk
+++ product/camera.mk
@@ -10,4 +10,5 @@ PRODUCT_PACKAGES += \
 
 # Camera properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    camera2.portability.force_api=1
+    camera2.portability.force_api=1 \
+    debug.camcorder.disablemeta=true
