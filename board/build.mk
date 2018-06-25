fafd78c796d0c65f8dc952fec305fc4ee69a75e1
Mathieu Chartier <mathieuc@google.com>
2018-06-25 04:07:02 +0200

    board: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3

--- board/build.mk
+++ board/build.mk
@@ -10,3 +10,6 @@ endif
 
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
