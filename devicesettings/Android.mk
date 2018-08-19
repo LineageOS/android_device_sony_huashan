6e9b112222744c60e11e288cb61f1d2a4fba0c47
Bruno Martins <bgcngm@gmail.com>
2018-08-19 01:54:37 +0100

    DeviceSettings: Properly depend on Lineage SDK
     * Platform apps that depend on the SDK must only link
       against org.lineageos.platform.internal
    
    Change-Id: I2e32c95e1e980879dc740ac63c14680e0de8a7da

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -25,7 +25,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
 
 ifeq ($(BOARD_AOSP_BASED),)
     LOCAL_STATIC_JAVA_LIBRARIES += \
-        org.lineageos.platform.sdk
+        org.lineageos.platform.internal
 endif
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
