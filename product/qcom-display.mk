0d7719aa495cba55fadd920cd8c4ebf87bd0d7e5
Arne Coucheron <arco68@gmail.com>
2018-11-28 02:14:57 +0100

    huashan: product: Add some HIDL services
    Change-Id: I28b422f1f7d746d8c42944ea6744d4094e78e349

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -10,8 +10,10 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service
 
 # OpenGL properties
 PRODUCT_PROPERTY_OVERRIDES += \
--- product/qcom-keymaster.mk
+++ product/qcom-keymaster.mk
@@ -1,3 +1,4 @@
 # Keymaster HIDL interfaces
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl
+    android.hardware.keymaster@3.0-impl \
+    android.hardware.keymaster@3.0-service
