fusion3-common/android_device_sony_fusion3-common

product: audio: Modify props to new audio HAL changes
Introduced into hardware/qcom/audio-caf
https://github.com/LineageOS/android_hardware_qcom_audio/commit/60a00a14edb6b10cc14e47097c2bb13ca3bcfc62

Change-Id: Iebd39f82cef69af4ee5c897ea118ff9d83b5a542

product/audio.mk
@@ -7,10 +7,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.disable=1 \
     media.aac_51_output_enabled=true \
-    persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.fluence.speaker=true \
     qcom.hw.aac.encoder=true \
     ro.config.media_vol_steps=25 \
     ro.config.vc_call_vol_steps=7 \
-    ro.qc.sdk.audio.fluencetype=fluence
+    ro.vendor.audio.sdk.fluencetype=fluence

LineageOS/android_device_sony_huashan

 * Introduced into hardware/qcom/audio-caf
   https://github.com/LineageOS/android_hardware_qcom_audio/commit/60a00a14edb6b10cc14e47097c2bb13ca3bcfc62

product/audio.mk
@@ -9,10 +9,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     audio.offload.disable=1 \
     mm.enable.smoothstreaming=true \
-    persist.audio.fluence.voicecall=true \
-    persist.audio.fluence.voicerec=false \
-    persist.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.fluence.speaker=true \
     qcom.hw.aac.encoder=true \
     ro.config.media_vol_steps=25 \
     ro.config.vc_call_vol_steps=7 \
-    ro.qc.sdk.audio.fluencetype=none
+    ro.vendor.audio.sdk.fluencetype=none