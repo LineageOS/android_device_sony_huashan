037a8cf6967f4e0c6ab729dfa3f43487542d0985
dianlujitao <dianlujitao@lineageos.org>
2019-05-14 15:22:19 +0800

    lineagehw: Convert HighTouchSensitivity to touch HAL
    Change-Id: Ibd8ba796cc2d44dd6b06f425ce6bd405850e25eb

--- board/hardware.mk
+++
@@ -1,5 +0,0 @@
-# Lineage Hardware
-ifeq ($(BOARD_AOSP_BASED),)
-BOARD_HARDWARE_CLASS += \
-    $(DEVICE_PATH)/lineagehw
-endif
---
+++ product/touchscreen.mk
@@ -0,0 +1,5 @@
+# Touchscreen packages
+ifeq ($(BOARD_AOSP_BASED),)
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.huashan
+endif
