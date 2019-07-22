1b432a37c3b97f53c1a501282dcb5823a529a343
Ady Abraham <adyabr@google.com>
2019-07-22 13:11:45 -0700

    display: Enable GL comp backpressure
    Enable backpressure for client composition to avoid jank due to
    HWC queue stuffing.
    
    Test: 1. Open Camera
          2. Swipe horizontally to go back to previous app.
    
    Bug: 138083790
    Change-Id: I3d26aa470c1660b23bd97133f39858f92638aca8

--- product/display.mk
+++ product/display.mk
@@ -1,5 +1,6 @@
 # Display properties
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.hwui.use_buffer_age=false \
+    debug.sf.enable_gl_backpressure=1 \
     persist.hwc.mdpcomp.enable=true \
     ro.sf.lcd_density=320
