1af17e6efddcc522b213fa7040c4a722df4bbe9d
Adrian DC <radian.dc@gmail.com>
2019-08-17 11:10:53 +0200

    product: Add exFAT and NTFS filesystem tools for AOSP builds
    Change-Id: Ib04d31a7069040ae6b801c4159927ac32ff044d3

--- product/aosp.mk
+++ product/aosp.mk
@@ -1,6 +1,15 @@
 # AOSP specific products
 ifneq ($(BOARD_AOSP_BASED),)
 
+# AOSP filesystems
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    fsck.ntfs \
+    mke2fs \
+    mkfs.exfat \
+    mkfs.ntfs \
+    mount.ntfs
+
 # AOSP packages
 PRODUCT_PACKAGES += \
     Launcher3 \
