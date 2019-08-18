30246e8639ad4d3c776cb672a5ef7b723be82c26
Adrian DC <radian.dc@gmail.com>
2019-08-18 15:40:42 +0200

    sepolicy: Clean sepolicies rules defined in qcom/sepolicy-legacy
     * Also perform minor codestyle improvements
    
    Change-Id: I264c3979d9a4fb97b6950ef299648b921de9f319

--- board/selinux.mk
+++ board/selinux.mk
@@ -4,9 +4,3 @@ include device/qcom/sepolicy-legacy/sepolicy.mk
 # Device sepolicies
 BOARD_SEPOLICY_DIRS += \
     $(DEVICE_PATH)/sepolicy
-
-# LineageOS device sepolicies
-ifeq ($(BOARD_AOSP_BASED),)
-BOARD_SEPOLICY_DIRS += \
-    $(DEVICE_PATH)/sepolicy-lineage
-endif
