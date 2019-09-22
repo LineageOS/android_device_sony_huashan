7476d1bfe9de39fd442ac0d2a31e2166acc47d5f
Adrian DC <radian.dc@gmail.com>
2019-09-22 23:35:38 +0200

    board: Revert "Speed profile services and wifi-service to reduce RAM and storage"
    Change-Id: Iea1325dfe608a472ce7a0cc7c8d99ae5eeba4d2d

--- board/build.mk
+++ board/build.mk
@@ -10,6 +10,3 @@ endif
 
 # Reduce system image size by limiting java debug info.
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-
-# Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
