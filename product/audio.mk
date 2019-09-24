aa1335f37cd201b8c28d8ff57fab9688798c247f
Arne Coucheron <arco68@gmail.com>
2019-09-25 01:48:08 +0200

    audio: Convert audio_policy.conf to xml
     * Adapt for Huashan configurations with deep_buffer
    
    Change-Id: I4c1179674c30d4858a57c3c0af07acb47367ff9c
    Original-Change-Id: Iee25dd4570445c32528de3707d27c00ede9d93b7
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/audio.mk
+++ product/audio.mk
@@ -2,7 +2,7 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Audio properties
