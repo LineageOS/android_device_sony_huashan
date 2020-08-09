6a0421d884a4417d5c8ac48270415a2d5e29f037
Adrian DC <radian.dc@gmail.com>
2020-08-09 16:34:27 +0200

    gps: Migrate gps.conf compilation from copy to target
     * Also align rootdir/Android.mk syntax and codestyle
    
    Change-Id: Ic9e808ee5685014f216b8e06b8e7e14151f6bd12

--- product/gps.mk
+++ product/gps.mk
@@ -1,6 +1,6 @@
-# GPS configurations
-PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf
+# GPS packages
+PRODUCT_PACKAGES += \
+    gps.conf
 
 # SEC configurations
 PRODUCT_COPY_FILES += \
