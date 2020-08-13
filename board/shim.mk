be8f3fe15b6a302dca63345838045a029353afb6
Adrian DC <radian.dc@gmail.com>
2020-08-13 23:02:25 +0200

    libshims: resolve legacy camera ILL_OPC signals from mediaserver
     * The libc library now includes libdl_android,
       which panics upon __aeabi_unwind_cpp_pr0 calls
    
     * Our legacy camera libraries trigger unwind events through
       debugging calls to Unwind_Backtrace and Unwind_VRS_Get
    
     * Instead of disabling __aeabi_unwind_cpp_pr0 globally,
       this commit defines a stub __gnu_Unwind_Backtrace
       used by the mediaserver only through SHIM
    
    Change-Id: I9ae18d1ac598fcc72a8971842a8d79dc9aed2123
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- board/shim.mk
+++ board/shim.mk
@@ -1,4 +1,5 @@
 # SHIM
 TARGET_LD_SHIM_LIBS := \
+    /system/bin/mediaserver|libshim_unwind.so \
     /system/lib/libcald_pal.so|libshim_cald.so \
     /system/lib/hw/camera.vendor.qcom.so|libshim_camera.so
