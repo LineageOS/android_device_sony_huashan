964a230825abb3b7c300a4b131111b55bd8104a3
Adrian DC <radian.dc@gmail.com>
2020-08-13 16:31:42 +0200

    product: move sensors 'sap.conf' back in /system for GPS binaries
     * Wrongly moved for Ie616e5023cdf5276edf6b0682bd86602d40549dd
       although they are used in the GPS prebuilt binaries
    
    Change-Id: I374e9e50c4b73db697ff43c1942d2574ecb1f076

--- product/sensors.mk
+++ product/sensors.mk
@@ -1,6 +1,6 @@
 # Sensors configurations
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/configs/sap.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/sap.conf \
     $(LOCAL_PATH)/configs/sensors.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors.conf
 
 # Sensors packages
